87700f36f9798253ef3afa2f609733d673e71c37
NISHANRAI111 <nishanrai125@gmail.com>
2025-04-03 17:15:21 +0545

    initial commit

--- Action/get_receptionists.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once '../database/database_connection.php';
-
-$stmt = $pdo->query("SELECT user_id, full_name, age, work_shift FROM receptionists");
-$receptionists = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode(['status' => $receptionists ? 'success' : 'empty', 'receptionists' => $receptionists]);
-?>
